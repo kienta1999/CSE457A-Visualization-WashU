63b37d6a787b1fc68dd5f67acf1615efaf6f0ddc
Kien Ta <talekien1710@gmail.com>
2021-10-14 19:47:01 -0500

    stopword removed and similarity score calculated

--- assignments/KienTa_a3/Readme.md
+++ assignments/KienTa_a3/Readme.md
@@ -1,1 +1,2 @@
 https://www.jasondavies.com/wordcloud/
+https://www.geeksforgeeks.org/removing-stop-words-nltk-python/
--- assignments/KienTa_a3/data_preprocess.py
+++ assignments/KienTa_a3/data_preprocess.py
@@ -1,6 +1,15 @@
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 from bs4 import BeautifulSoup
 from collections import Counter
 import json
+import nltk
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+wnl = WordNetLemmatizer()
 
 
 def string_preprocess(input_str):
@@ -11,7 +20,12 @@ def string_preprocess(input_str):
             ans += ' '
         elif char not in punctuations:
             ans += char
-    return ans.lower().strip()
+    ans = ans.lower().strip()
+    # Filter stopword
+    word_tokens = word_tokenize(ans)
+    filtered_sentence = [wnl.lemmatize(w) for w in word_tokens if not w.lower() in set(
+        stopwords.words('english'))]
+    return ' '.join(filtered_sentence)
 
 
 def str_to_freq(str):
@@ -20,6 +34,24 @@ def str_to_freq(str):
     for word in counter:
         ans.append({'word': word, 'frequency': counter[word]})
     return ans
+
+
+def similarity_score(word_frequency1, word_frequency2):
+    words = [word_freq['word'] for word_freq in word_frequency1]
+    words.extend([word_freq['word'] for word_freq in word_frequency2])
+    words = set(words)
+    dot_product = 0
+    dis1 = 0
+    dis2 = 0
+    for word in words:
+        freq1 = list(filter(lambda x: (x['word'] == word), word_frequency1))
+        freq1 = freq1[0]["frequency"] if freq1 else 0
+        freq2 = list(filter(lambda x: (x['word'] == word), word_frequency2))
+        freq2 = freq2[0]["frequency"] if freq2 else 0
+        dot_product += freq1 * freq2
+        dis1 += freq1 ** 2
+        dis2 += freq2 ** 2
+    return dot_product / ((dis1 * dis2) ** 0.5)
 
 
 f = open('englishfairytales/englishfairytales.html', 'r')
@@ -39,6 +71,15 @@ for i, h2 in enumerate(all_h2):
         title_and_content.append(obj)
 for element in title_and_content:
     element['word_frequency'] = str_to_freq(element['original_content'])
+
+for element in title_and_content:
+    element['similarity_score'] = []
+    for element2 in title_and_content:
+        if element != element2:
+            score = similarity_score(
+                element['word_frequency'], element2['word_frequency'])
+            element['similarity_score'].append(
+                {'title': element2["title"], 'score': score})
 f = open("processed_englishfairytales.json", "w")
 f.write(json.dumps(title_and_content))
 f.close()
